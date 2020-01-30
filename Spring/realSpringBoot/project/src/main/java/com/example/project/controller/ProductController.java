d08f98b342818e75a161f66f4cd2eeda5d3fabd1
Jorge Luis Neves Aranha - JAA <jorge.aranha@iteris.com.br>
2020-01-30 17:15:53 -0300

    push que eu nao dei

--- Spring/realSpringBoot/project/src/main/java/com/example/project/controller/ProductController.java
+++ Spring/realSpringBoot/project/src/main/java/com/example/project/controller/ProductController.java
@@ -1,11 +1,16 @@
 package com.example.project.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 
 import com.example.project.domain.Product;
 import com.example.project.service.ProductService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,8 +36,26 @@ public class ProductController {
 		return ResponseEntity.ok(productService.findById(id));
 	}
 
+	@GetMapping(value = "image={id}")
+	public ResponseEntity<?> getImageById(@PathVariable Integer id) {
+		Resource file = productService.downloadFile(id);
+		
+		String mimeType = "";
+		
+		try {
+			mimeType = Files.probeContentType(file.getFile().toPath());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return ResponseEntity.ok().contentType(MediaType.parseMediaType(mimeType))
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
+				.body(file);
+	}
+
 	@PutMapping(value = "image={id}")
-	public ResponseEntity putMethodName(@PathVariable Integer id, MultipartFile file) {
+	public ResponseEntity<?> putMethodName(@PathVariable Integer id, MultipartFile file) {
 		return ResponseEntity.ok(productService.uploadFile(file, id));
 	}
+
 }
--- Spring/realSpringBoot/project/src/main/java/com/example/project/service/ProductService.java
+++ Spring/realSpringBoot/project/src/main/java/com/example/project/service/ProductService.java
@@ -1,8 +1,9 @@
 package com.example.project.service;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.List;
@@ -14,6 +15,9 @@ import com.example.project.exception.DataNotFoundException;
 import com.example.project.repository.ProductRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -25,32 +29,8 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    private String path = "C:\\Users\\jorge.aranha\\Pictures";
-
-    public String uploadFile(MultipartFile file, Integer id) {
-
-        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-
-        if (file.isEmpty()) {
-            throw new BusinessRuleException("Failed to store empty file");
-        }
-
-        try {
-            String path = "C:\\Users\\jorge.aranha\\Pictures\\";
-			Files.copy(file.getInputStream(), Paths.get(path), StandardCopyOption.REPLACE_EXISTING);
-		} catch (IOException e) {
-			e.printStackTrace();
-        }
-
-        String image = path + "\\" + fileName;
-        Product product = findById(id);
-        product.setImage(image);
-
-        productRepository.save(product);
-
-        return image;
-
-    }
+    private String path = "/home/jorgearanha/Pictures/";
+    // private String path = "C:\\Users\\jorge.aranha\\Pictures\\";
 
     public List<Product> list() {
         return productRepository.findAll();
@@ -60,5 +40,47 @@ public class ProductService {
         Optional<Product> product = productRepository.findById(id);
         return product.orElseThrow(() -> new DataNotFoundException("Product not found"));
     }
-    
+
+    public String uploadFile(MultipartFile file, Integer id) {
+
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+        String image = path + "/" + fileName;
+        // String image = path + "\\" + fileName;
+
+        if (file.isEmpty()) {
+            throw new BusinessRuleException("Failed to store empty file");
+        }
+
+        try {
+            Files.copy(file.getInputStream(), Paths.get(image), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Product product = findById(id);
+        product.setImage(image);
+
+        productRepository.save(product);
+
+        return image;
+
+    }
+
+    public Resource downloadFile(Integer id) {
+
+        Product product = findById(id);
+
+        Path path = Paths.get(product.getImage());
+        Resource resource = null;
+
+        try {
+            resource = new UrlResource(path.toUri());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return resource;
+    }
+
 }
